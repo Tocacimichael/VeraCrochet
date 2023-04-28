9b97e8f236101c6e100c71732ecb2582d3965e9b
StonerCoder <118166319+Tocacimichael@users.noreply.github.com>
2023-04-28 13:37:18 +0200

    First needs

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Vera Cochet </title>
+        <link rel="stylesheet" href="./resources/Custom/style.css">
+        <link rel="stylesheet" href="./resources/bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
+        <script src="https://ajax.com.googleapis.com/ajax/libs/jquery/.min.js"></script>
+        <script src="./resources/bootstrap-5.3.0-alpha3-dist/js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./resources/imgs/Logo_VCC.png" class="w-25"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav" id="navbar_nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                    </li>
+
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner" id="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="./resources/imgs/Bee's.jpeg" class="d-block w-50 ml-" >
+                    </div>
+                    <div class="carousel-item">
+                    <img src="./resources/imgs/Cherry_0.1.jpeg" class="d-block w-50" >
+                    </div>
+                    <div class="carousel-item">
+                    <img src="./resources/imgs/Strawberry_0.1.jpeg" class="d-block w-50" >
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+             <div class="aboutMe">
+                <h1 style="margin: 5% 0 5% 0; text-align: center;">About Me</h1>
+                <p style="margin: 5% 0 5% 0; text-align: center;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla eget lectus at lobortis. <br>
+                Duis egestas erat semper lacus tristique, vitae mattis magna eleifend. Proin aliquam semper lorem, <br>
+                in sagittis risus luctus ut. Nam id magna molestie quam lobortis pulvinar suscipit at diam. Fusce aliquet, <br>
+                elit eu vulputate rhoncus, magna erat placerat massa, eget facilisis purus ligula ut lacus. Donec mauris mauris, <br>
+                dignissim tristique volutpat ut, sollicitudin a est. Etiam ante est, placerat in mollis a, tempor et dolor. <br>
+                Proin consectetur, enim eget aliquet sollicitudin, neque libero consectetur sem, in auctor mi tellus ut massa. <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a pulvinar dolor. Pellentesque lorem orci, <br>
+                sodales id quam eget, blandit consectetur metus. Donec luctus vulputate eros, in vehicula leo placerat id.<br>
+                </p>
+
+
+                <img src="./resources/imgs/ProfilePic.png" class="d-block w-25 " style="margin: 0 0 5% 38%;">
+             </div>
+
+
+
+
+
+
+        <div class="container text-center">
+            <div class="row row-cols-2">
+                <div class="col" style="margin: 0 5% 0 -10%;">
+                    <img src="./resources/imgs/Bags.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 -5% 0 10%;">
+                    <img src="./resources/imgs/Cherry_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 5% 0 -10%;">
+                    <img src="./resources/imgs/Frog_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 -5% 0 10%;">
+                    <img src="./resources/imgs/Paddo_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 5% 0 -10%;">
+                    <img src="./resources/imgs/Dinos.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 -5% 0 10%;">
+                    <img src="./resources/imgs/smallPaddo_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 5% 0 -10%;">
+                    <img src="./resources/imgs/Strawberry_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+                <div class="col" style="margin: 0 -5% 0 10%;">
+                    <img src="./resources/imgs/Snorlax_0.1.jpeg">
+                    <h2> Bags </h2>
+                    <h2>€ 14,99 </h2>
+                </div>
+            </div>
+        </div>
+</div>
+    
+    </body>
+</html>
