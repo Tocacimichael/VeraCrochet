dc9b712590c880246904704a076ebc392f245cb1
StonerCoder <tocacimichael@gmail.com>
2023-04-29 18:40:56 +0200

    new stuff 29-04-2022

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </head>
     <body>
 
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-sm" style=" z-index: 20;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#"><img src="./resources/imgs/Logo_VCC.png" class="w-25"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,7 +29,6 @@
                     <li class="nav-item">
                     <a class="nav-link" href="#">Pricing</a>
                     </li>
-
                 </ul>
                 </div>
             </div>
@@ -57,62 +56,60 @@
                 </button>
             </div>
 
-             <div class="aboutMe">
+             <div class="aboutMe" class="text-center">
                 <h1 style="margin: 5% 0 5% 0; text-align: center;">About Me</h1>
-                <p style="margin: 5% 0 5% 0; text-align: center;">
+                <p style="text-align: center; width: 80%;">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla eget lectus at lobortis. <br>
                 Duis egestas erat semper lacus tristique, vitae mattis magna eleifend. Proin aliquam semper lorem, <br>
                 in sagittis risus luctus ut. Nam id magna molestie quam lobortis pulvinar suscipit at diam. Fusce aliquet, <br>
                 elit eu vulputate rhoncus, magna erat placerat massa, eget facilisis purus ligula ut lacus. Donec mauris mauris, <br>
                 dignissim tristique volutpat ut, sollicitudin a est. Etiam ante est, placerat in mollis a, tempor et dolor. <br>
-                Proin consectetur, enim eget aliquet sollicitudin, neque libero consectetur sem, in auctor mi tellus ut massa. <br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a pulvinar dolor. Pellentesque lorem orci, <br>
-                sodales id quam eget, blandit consectetur metus. Donec luctus vulputate eros, in vehicula leo placerat id.<br>
+                Proin consectetur, enim eget aliquet sollicitudin, neque libero consectetur sem, in auctor mi tellus ut massa.
                 </p>
 
 
-                <img src="./resources/imgs/ProfilePic.png" class="d-block w-25 " style="margin: 0 0 5% 38%;">
+                <img src="./resources/imgs/ProfilePicEdit.png" class="img-fluid img-thumbnail">
              </div>
 
         <div class="container text-center">
             <div class="row row-cols-2">
-                <div class="col" style="margin: 0 5% 0 -10%;">
-                    <img src="./resources/imgs/Bags.jpeg">
+                <div class="col" >
+                    <img src="./resources/imgs/Bags.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 -5% 0 10%;">
-                    <img src="./resources/imgs/Cherry_0.1.jpeg">
+                <div class="col">
+                    <img src="./resources/imgs/Cherry_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 5% 0 -10%;">
-                    <img src="./resources/imgs/Frog_0.1.jpeg">
+                <div class="col" >
+                    <img src="./resources/imgs/Frog_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 -5% 0 10%;">
-                    <img src="./resources/imgs/Paddo_0.1.jpeg">
+                <div class="col">
+                    <img src="./resources/imgs/Paddo_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 5% 0 -10%;">
-                    <img src="./resources/imgs/Dinos.jpeg">
+                <div class="col" >
+                    <img src="./resources/imgs/Dinos.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 -5% 0 10%;">
-                    <img src="./resources/imgs/smallPaddo_0.1.jpeg">
+                <div class="col">
+                    <img src="./resources/imgs/smallPaddo_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 5% 0 -10%;">
-                    <img src="./resources/imgs/Strawberry_0.1.jpeg">
+                <div class="col" >
+                    <img src="./resources/imgs/Strawberry_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
-                <div class="col" style="margin: 0 -5% 0 10%;">
-                    <img src="./resources/imgs/Snorlax_0.1.jpeg">
+                <div class="col">
+                    <img src="./resources/imgs/Snorlax_0.1.jpeg" class="img-fluid img-thumbnail">
                     <h2> Bags </h2>
                     <h2>€ 14,99 </h2>
                 </div>
